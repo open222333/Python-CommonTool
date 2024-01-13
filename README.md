388d844e2e0f749db6b57e7cb0a709f0d60df85b
TomLi <open222333@gmail.com>
2024-01-13 16:45:54 +0800

    新增 常用工具

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
-# Template-PythonRepo
+# Python-CommonTool
 
 ```
-我的專案樣本 Python
+Python 自製常用工具 - 進度條, Log
 ```
 
 ## 目錄
 
-- [Template-PythonRepo](#template-pythonrepo)
+- [Python-CommonTool](#python-commontool)
 	- [目錄](#目錄)
 	- [參考資料](#參考資料)
-		- [book： `書` 相關](#book-書-相關)
-- [資料夾對應書本名稱](#資料夾對應書本名稱)
 - [配置文檔](#配置文檔)
 - [用法](#用法)
 
@@ -18,21 +16,35 @@
 
 []()
 
-### book： `書` 相關
-
-[]()
-
-# 資料夾對應書本名稱
-
-book： `書`
-
-
 # 配置文檔
 
-```
+```ini
+[LOG]
+; 預設為關閉 => 0: 關閉 1: 開啟 測試函式
+; TEST_FUNCTION=1
+
+; 關閉log功能 輸入選項 (true, True, 1) 預設 不關閉
+; LOG_DISABLE=1
+
+; logs路徑 預設 logs
+; LOG_PATH=
+
+; 關閉紀錄log檔案 輸入選項 (true, True, 1)  預設 不關閉
+; LOG_FILE_DISABLE=1
+
+; 設定紀錄log等級 DEBUG,INFO,WARNING,ERROR,CRITICAL 預設WARNING
+; LOG_LEVEL=
+
+; 指定log大小(輸入數字) 單位byte, 與 LOG_DAYS 只能輸入一項 若都輸入 LOG_SIZE優先
+; LOG_SIZE=
+
+; 指定保留log天數(輸入數字) 預設7
+; LOG_DAYS=
 ```
 
 # 用法
+
+Log
 
 ```
 ```
--- src/__init__.py
+++ src/__init__.py
@@ -3,4 +3,24 @@ import os
 
 
 config = ConfigParser()
-config.read(os.path.join('conf', 'config.ini'))
+config.read(os.path.join('conf', 'configig.ini'))
+
+
+# logs相關參數
+# 關閉log功能 輸入選項 (true, True, 1) 預設 不關閉
+LOG_DISABLE = config.getboolean('LOG', 'LOG_DISABLE', fallback=False)
+# logs路徑 預設 logs
+LOG_PATH = config.get('LOG', 'LOG_PATH', fallback='logs')
+# 設定紀錄log等級 DEBUG,INFO,WARNING,ERROR,CRITICAL 預設WARNING
+LOG_LEVEL = config.get('LOG', 'LOG_LEVEL', fallback='WARNING')
+# 關閉紀錄log檔案 輸入選項 (true, True, 1)  預設 不關閉
+LOG_FILE_DISABLE = config.getboolean('LOG', 'LOG_FILE_DISABLE', fallback=False)
+# 指定log大小(輸入數字) 單位byte, 與 LOG_DAYS 只能輸入一項 若都輸入 LOG_SIZE優先
+LOG_SIZE = config.getint('LOG', 'LOG_SIZE', fallback=0)
+# 指定保留log天數(輸入數字) 預設7
+LOG_DAYS = config.getint('LOG', 'LOG_DAYS', fallback=7)
+
+if not LOG_FILE_DISABLE:
+    # 建立log資料夾
+    if not os.path.exists(LOG_PATH) and not LOG_DISABLE:
+        os.makedirs(LOG_PATH)
